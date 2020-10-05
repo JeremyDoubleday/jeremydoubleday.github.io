10bae379408aa3e90ab61599bd73f2c976129d30
Jeremy Doubleday <phate6983@gmail.com>
2020-10-05 13:18:08 -0500

    updating with images starting json manipulation

---
+++ mainScripts.js
@@ -0,0 +1,22 @@
+"use strict";
+
+window.onload = init;
+
+function init() {
+    loadSurvs(function(response) {
+        var jasonSurvs = JSON.parse(response);
+    });
+    console.log(jasonSurvs);
+}
+
+function loadSurvs(callback) {
+    var obj = new XMLHttpRequest();
+    obj.overrideMimeType("application/json");
+    obj.open('GET', 'allSurvs.json', true);
+    obj.onreadystatechange = function () {
+        if (obj.readyState == 4 && obj.status == "200") {
+            callback(obj.responseText);
+        }
+    };
+    obj.send(null);
+}
