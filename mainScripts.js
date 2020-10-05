844015d5b2d9a327e56c7279c82aec3cc7231a19
Jeremy Doubleday <phate6983@gmail.com>
2020-10-05 15:18:45 -0500

    updating survivor perks page

--- mainScripts.js
+++ mainScripts.js
@@ -1,24 +1,49 @@
 "use strict";
 
 window.onload = init;
-var jasonSurvs = null;
 
 
 function init() {
-    loadSurvs(function(response) {
-        jasonSurvs = JSON.parse(response);
-    });
-    console.log(jasonSurvs);
+    
 }
 
-function loadSurvs(callback) {
-    var obj = new XMLHttpRequest();
-    obj.overrideMimeType("application/json");
-    obj.open('GET', 'allSurvs.json', true);
-    obj.onreadystatechange = function () {
-        if (obj.readyState == 4 && obj.status == "200") {
-            callback(obj.responseText);
+function runSPerks() {
+    document.getElementById("mainBody").innerHTML = createSurvTable();
+}
+
+function runKPerks() {
+    console.log(allKills);
+}
+
+function createSurvTable() {
+    var htmlString = "<table id='survTable'>";
+    for (var i = 0; i < allSurvs.length; i++) {
+        if (i % 3 === 0) {
+            if (i > 0) {
+                htmlString += "</tr>";
+            }
+            htmlString += "<tr>";
         }
-    };
-    obj.send(null);
+        htmlString += "<td><img src='" + allSurvs[i].icon.preview_portrait + "' /></td><td>"
+        htmlString += "<table class='perkTable'>";
+        for (var j = 0; j < 3; j++) {
+            var temp = allSurvs[i].perks[j];
+            var perkImage = findPerkLink(temp);
+            htmlString += "<tr><td><img id='perkImage' src='" + perkImage + "' />" + temp + "</td></tr>";
+        }
+        htmlString += "</table>";
+    }
+    
+    htmlString += "</table></td>";
+    document.getElementById("mainBody").style.height = "1750px";
+    document.getElementById("mainBody").style.width = "1050px";
+    return htmlString;
+}
+
+function findPerkLink(temp) {
+    for (var i = 0; i < allPerks.length; i++) {
+        if (allPerks[i].perk_tag === temp) {
+            return allPerks[i].icon;
+        }
+    }
 }
